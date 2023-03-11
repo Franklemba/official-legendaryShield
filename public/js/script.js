438febed5b0592e9e9332777d2a94c473fe82978
Frank <franklembalemba3@gmail.com>
2023-03-11 02:53:50 +0200

    searchBar update

--- public/js/script.js
+++ public/js/script.js
@@ -3,22 +3,42 @@
 let bar = document.getElementById('bar');
 let navbar = document.getElementById('navbar');
 let overlay = document.querySelector('.overlay');
-
+let searchBar = document.getElementById('searchBar');
+let search = document.querySelector('.search');
+let check = true
 
 bar.addEventListener('click',()=>{
     navbar.classList.add('active');
     overlay.classList.add('active');
+    search.classList.remove('active');
+    check = true;
     // alert("kejnskvjk")
 })
 
 overlay.addEventListener('click',()=>{
     navbar.classList.remove('active');
     overlay.classList.remove('active');
+    search.classList.remove('active');
+    check = true;
 })
 
 // console.log("what the heck is going on here")
 
+searchBar.addEventListener('click',()=>{
+    if(check == true){
+        search.classList.add('active');
+        check = false;
+    }else if(check == false){
+        search.classList.remove('active');
+        check = true;
+    }
+})
+
+
+
 window.onscroll =() =>{
     navbar.classList.remove('active');
     overlay.classList.remove('active');
+    search.classList.remove('active');
+    check = true;
 }
