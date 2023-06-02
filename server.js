45e351250a6a47ea5f94d6c57a88cf9bc1c178ca
Frank <franklembalemba3@gmail.com>
2023-06-02 12:06:39 +0200

    included neck chain

--- server.js
+++ server.js
@@ -30,16 +30,16 @@ const mongoose = require("mongoose");
 //global database connection
    // online connection////
     mongoose.set('strictQuery', true);
-    mongoose.connect('mongodb+srv://franklemba:sharon@svintstore.q1axgo7.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true})
-    .then(()=>{
-        console.log('database is connected')
-    }).catch((err) => console.log(err));
+    // mongoose.connect('mongodb+srv://franklemba:sharon@svintstore.q1axgo7.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true})
+    // .then(()=>{
+    //     console.log('database is connected')
+    // }).catch((err) => console.log(err));
 // ////////local database connection
 
-// mongoose.connect(process.env.DATABASE_Url)
-// .then(()=>{
-//     console.log('database is connected')
-// }).catch((err) => console.log(err));
+mongoose.connect(process.env.DATABASE_Url)
+.then(()=>{
+    console.log('database is connected')
+}).catch((err) => console.log(err));
 
 
 app.use('/',indexRouter );
