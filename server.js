0b7f41e191bb097f4ff762d9e191853679596cc5
Awesome Charles <chisalecharles23@gmail.com>
2023-08-02 15:59:04 +0200

    getting there

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const mongoose = require("mongoose");
     ////local database connection
     
   
-    //mongoose.connect('mongodb://127.0.0.1:27017/legendary_shield')
+   //mongoose.connect('mongodb://127.0.0.1:27017/legendary_shield')
     mongoose.connect("mongodb+srv://franklemba:sharon@svintstore.q1axgo7.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true})
 .then(()=>{
     console.log('database is connected')
